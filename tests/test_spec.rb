8df1de857c140b01a156c3078d72b3be23b47d66
bernard <bernard@traveloka.com>
2018-03-15 19:44:48 +0700

    update test_spec.rb

--- tests/test_spec.rb
+++ tests/test_spec.rb
@@ -1,9 +1,3 @@
-describe package("amazon-ssm-agent") do
-  it { should be_installed }
-  it { should be_running }
-  it { should be_enabled }
-end
-
 describe service("amazon-ssm-agent") do
     it { should be_installed }
     it { should be_running }
