521cc67325de8e7827567aa3ba1ff3ce0e4138d7
HieronyM <hierony.manurung@traveloka.com>
2019-07-09 18:21:04 +0700

    Update inspect test to support ubuntu16

--- test/integration/default/test_spec.rb
+++ test/integration/default/test_spec.rb
@@ -1,4 +1,13 @@
-describe service("amazon-ssm-agent") do
-  it { should be_installed }
-  it { should be_enabled }
+if os.release == "14.04"
+  describe service("amazon-ssm-agent") do
+    it { should be_installed }
+    it { should be_enabled }
+  end
+end 
+
+if os.release == "16.04"
+  describe systemd_service("amazon-ssm-agent") do
+    it { should be_installed }
+    it { should be_enabled }
+  end
 end
